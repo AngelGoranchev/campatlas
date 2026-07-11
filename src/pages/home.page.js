88b141f789d04b34265148f94e41047432fa82e0
Angel Goranchev <www.amg.com@gmail.com>
2026-07-11 22:47:59 +0300

    fix(home): replace auth modals with real auth pages

--- src/pages/home.page.js
+++ src/pages/home.page.js
@@ -2,27 +2,3 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import '../styles/app.css';
-import { initAuthModals } from '../components/auth-modals.js';
-
-initAuthModals();
-
-
-function cleanupModalBackdrops() {
-  const hasOpenModal = document.querySelector('.modal.show');
-
-  if (hasOpenModal) {
-    return;
-  }
-
-  document.querySelectorAll('.modal-backdrop').forEach((backdrop) => {
-    backdrop.remove();
-  });
-
-  document.body.classList.remove('modal-open');
-  document.body.style.removeProperty('overflow');
-  document.body.style.removeProperty('padding-right');
-}
-
-document.addEventListener('hidden.bs.modal', () => {
-  window.setTimeout(cleanupModalBackdrops, 200);
-});
